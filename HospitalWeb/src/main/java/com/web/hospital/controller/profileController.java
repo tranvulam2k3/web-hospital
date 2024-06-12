f613d4783f53eabe1013668533667307e1093c08
tranvulam <tranvulam2392k3@gmail.com>
2024-06-12 21:14:13 +0700

    comfig 12/6

---
+++ HospitalWeb/src/main/java/com/web/hospital/controller/profileController.java
@@ -0,0 +1,13 @@
+package com.web.hospital.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class profileController {
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "myprofile";
+    }
+}
